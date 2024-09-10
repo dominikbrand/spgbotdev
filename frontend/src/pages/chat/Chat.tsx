8cc1d76641f1b7007168a8013d8d3585425e8ceb
dominikbrand <63984069+dominikbrand@users.noreply.github.com>
2024-09-10 10:22:55 +0200

    Erweiterung KB URL auf Startseite

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -793,7 +793,7 @@ const Chat = () => {
               <Stack className={styles.chatEmptyState}>
                 <img src={logo} className={styles.chatIcon} aria-hidden="true" />
                 <h1 className={styles.chatEmptyStateTitle}>{ui?.chat_title}</h1>
-                <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description}</h2>
+                <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description} <br> Brauchst du Hilfe? <a href='https://paraplegiech.sharepoint.com/sites/intra-knowledgebase-de/SitePages/SPGBotPrompting.aspx'>Klick hier </a></h2>
               </Stack>
             ) : (
               <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? '40px' : '0px' }} role="log">
